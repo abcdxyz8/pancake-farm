6b9814fe9a7f4a3d04a70b4dccb12d9b0a048cd1
Tom AI Dev Team <tom.ai.devteam@gmail.com>
2022-01-28 15:54:22 +0000

    only deploy masterChef

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,19 +4,22 @@ let MasterChef = artifacts.require("./MasterChef.sol")
 
 module.exports = async function (deployer, network, accounts) {
 
-    await deployer.deploy(CakeToken);
-    const CakeTokenAddr = await CakeToken.deployed();
-    console.log("CakeToken: ", CakeTokenAddr.address);
+    // await deployer.deploy(CakeToken);
+    // const CakeTokenAddr = await CakeToken.deployed();
+    // console.log("CakeToken: ", CakeTokenAddr.address);
 
-    await deployer.deploy(SyrupBar, CakeTokenAddr.address);
-    const SyrupBarAddr = await SyrupBar.deployed();
-    console.log("SyrupBar: ", SyrupBarAddr.address);
+    // await deployer.deploy(SyrupBar, CakeTokenAddr.address);
+    // const SyrupBarAddr = await SyrupBar.deployed();
+    // console.log("SyrupBar: ", SyrupBarAddr.address);
+    CakeTokenAddr = "0x8B6571C6D26dBa39be2263C35E3006A18312B328";
+    SyrupBarAddr = "0x3A20f2e7D4c4bb4A6A7053d51bC317f6046cd02a";
 
-    const cakePerBlock=40;
+
+    const cakePerBlock=40*1E18;
     const startBlock=703820;
     const devAddr="0x9892657D3A386661AF2fA989AE10b6916d43b93F";
-    await deployer.deploy(MasterChef, CakeTokenAddr.address, SyrupBarAddr.address, devAddr, cakePerBlock, startBlock);
-    const MasterChefAddr = await MasterChef.deployed();
-    console.log("MasterChef: ", MasterChefAddr.address);
+    await deployer.deploy(MasterChef, CakeTokenAddr, SyrupBarAddr, devAddr, cakePerBlock, startBlock);
+    const MasterChefAddr = await MasterChef.deployed().address;
+    console.log("MasterChef: ", MasterChefAddr);
 
 };
